9b89b77b55e0e83fe35bb3df6bd464302b1b7c06
Dali254 <dakatjk@gmail.com>
2026-06-30 03:43:04 +0300

    Delete pages directory

--- pages/api/pay-callback.js
+++
@@ -1,8 +0,0 @@
-// pages/api/pay-callback.js
-// Receives payment status from PayHero
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  // In production: verify signature, update DB, credit user wallet
-  console.log('PayHero callback:', JSON.stringify(req.body));
-  return res.status(200).json({ received: true });
-}
